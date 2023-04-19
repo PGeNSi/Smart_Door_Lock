165ae34d1b4118f1e20529badc1ffa9e5461279d
PunyapatKMUTNB <s6301012620057@email.kmutnb.ac.th>
2023-04-19 10:58:12 +0700

    Add task start serial print

--- src/main.cpp
+++ src/main.cpp
@@ -39,13 +39,21 @@ void setup(){
 
     // Initialize All Task
     Serial.println("> Creating Tasks");
+    Serial.println("--> Starting rtcTask Task");
     xTaskCreate(rtcAutoCalibrationTask, "rtcTask", TASK_RTC_STACK_DEPTH, NULL, TASK_RTC_PRIORITY, &taskHandle[0]);
+    Serial.println("--> Starting lcdTask Task");
     xTaskCreate(lcdTask, "lcdTask", TASK_LCD_STACK_DEPTH, NULL, TASK_LCD_PRIORITY, &taskHandle[1]);
+    Serial.println("--> Starting keypadTask Task");
     xTaskCreate(keypadTask, "keypadTask", TASK_KEYPAD_STACK_DEPTH, NULL, TASK_KEYPAD_PRIORITY, &taskHandle[2]);
+    Serial.println("--> Starting rfidTask Task");
     xTaskCreate(rfidTask, "rfidTask", TASK_RFID_STACK_DEPTH, NULL, TASK_RFID_PRIORITY, &taskHandle[3]);
+    Serial.println("--> Starting modeNormalTask Task");
     xTaskCreate(modeNormalTask, "modeNormalTask", TASK_MODENORMAL_STACK_DEPTH, NULL, TASK_MODENORMAL_PRIORITY, &taskHandle[4]);
+    Serial.println("--> Starting modeAdminTask Task");
     xTaskCreate(modeAdminTask, "modeAdminTask", TASK_MODEADMIN_STACK_DEPTH, NULL, TASK_MODEADMIN_PRIORITY, &taskHandle[5]);
+    Serial.println("--> Starting relayTask Task");
     xTaskCreate(relayTask, "relayTask", TASK_RELAY_STACK_DEPTH, NULL, TASK_RELAY_PRIORITY, &taskHandle[6]);
+    Serial.println("--> Starting AMTSTask Task");
     xTaskCreate(adminModeTriggeringSystemTask, "AMTSTask", TASK_ADMINMODETRIGGERER_STACK_DEPTH, NULL, TASK_ADMINMODETRIGGERER_PRIORITY, &taskHandle[7]);
     Serial.println("> Tasks Created");
 
